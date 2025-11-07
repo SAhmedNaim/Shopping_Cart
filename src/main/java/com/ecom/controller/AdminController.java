8a56abf02d414cebf39286cfd7fa7ccb3b099fb8
Shouvick Ahmed Naim <naim.ahmed035@gmail.com>
2025-11-07 15:47:37 +0600

    Feat: Module Product

--- src/main/java/com/ecom/controller/AdminController.java
+++ src/main/java/com/ecom/controller/AdminController.java
@@ -1,8 +1,12 @@
 package com.ecom.controller;
 
 import com.ecom.model.Category;
+import com.ecom.model.Product;
 import com.ecom.service.CategoryService;
+import com.ecom.service.ProductService;
+
 import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
@@ -10,8 +14,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -26,6 +34,9 @@ public class AdminController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("/")
     public String index() {
@@ -129,5 +140,72 @@ public class AdminController {
         }
 
         return "redirect:/admin/loadEditCategory/" + category.getId();
+    }
+
+    @PostMapping("/saveProduct")
+    public String saveProduct(@ModelAttribute Product product, @RequestParam("file") MultipartFile image, HttpSession session) throws IOException {
+
+        String imageName = image.isEmpty() ? "default.jpg" : image.getOriginalFilename();
+
+        product.setImage(imageName);
+
+        Product saveProduct = productService.saveProduct(product);
+
+        if (!ObjectUtils.isEmpty(saveProduct)) {
+
+            File saveFile = new ClassPathResource("static/img").getFile();
+
+            Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "product_img" + File.separator + image.getOriginalFilename());
+
+            System.out.println(path);
+            Files.copy(image.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+            session.setAttribute("succMsg", "Product Saved Success");
+        } else {
+            session.setAttribute("errorMsg", "something wrong on server");
+        }
+
+        return "redirect:/admin/loadAddProduct";
+    }
+
+    @GetMapping("/products")
+    public String loadViewProduct(Model m) {
+        m.addAttribute("products", productService.getAllProducts());
+        return "admin/products";
+    }
+
+    @GetMapping("/deleteProduct/{id}")
+    public String deleteProduct(@PathVariable int id, HttpSession session) {
+        Boolean deleteProduct = productService.deleteProduct(id);
+
+        if (deleteProduct) {
+            session.setAttribute("succMsg", "Product delete success");
+        } else {
+            session.setAttribute("errorMsg", "Something wrong on server");
+        }
+
+        return "redirect:/admin/products";
+    }
+
+    @GetMapping("/editProduct/{id}")
+    public String editProduct(@PathVariable int id, Model m) {
+        m.addAttribute("product", productService.getProductById(id));
+        m.addAttribute("categories", categoryService.getAllCategory());
+
+        return "admin/edit_product";
+    }
+
+    @PostMapping("/updateProduct")
+    public String updateProduct(@ModelAttribute Product product, @RequestParam("file") MultipartFile image, HttpSession session, Model m) {
+
+        Product updateProduct = productService.updateProduct(product, image);
+
+        if (!ObjectUtils.isEmpty(updateProduct)) {
+            session.setAttribute("succMsg", "Product update success");
+        } else {
+            session.setAttribute("errorMsg", "Something wrong on server");
+        }
+
+        return "redirect:/admin/editProduct/" + product.getId();
     }
 }
