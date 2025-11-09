0c7ee6606ea54932adb0ba1b875264fcc91b7d2c
Shouvick Ahmed Naim <naim.ahmed035@gmail.com>
2025-11-10 01:48:24 +0600

    Feat: Add to Cart

--- src/main/java/com/ecom/controller/UserController.java
+++ src/main/java/com/ecom/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.ecom.controller;
 
+import com.ecom.model.Cart;
 import com.ecom.model.Category;
 import com.ecom.model.User;
+import com.ecom.service.CartService;
 import com.ecom.service.CategoryService;
 import com.ecom.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 import java.util.List;
@@ -23,6 +28,9 @@ public class UserController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @Autowired
+    private CartService cartService;
 
     @GetMapping("/")
     public String home() {
@@ -39,6 +47,18 @@ public class UserController {
 
         List<Category> allActiveCategory = categoryService.getAllActiveCategory();
         m.addAttribute("categories", allActiveCategory);
+    }
+
+    @GetMapping("/addCart")
+    public String addToCart(@RequestParam Integer pid, @RequestParam Integer uid, HttpSession session) {
+        Cart saveCart = cartService.saveCart(pid, uid);
+
+        if (ObjectUtils.isEmpty(saveCart)) {
+            session.setAttribute("errorMsg", "Product add to cart failed");
+        }else {
+            session.setAttribute("succMsg", "Product added to cart");
+        }
+        return "redirect:/product/" + pid;
     }
 
 }
